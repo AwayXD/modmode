6f6a2091397f6cf7b1cd4e8729937bb32775640f
Stryff <cjkester8@gmail.com>
2024-08-12 03:36:19 -0400

    cock

--- src/main/java/org/awayxd/modmode/listeners/JoinListener.java
+++ src/main/java/org/awayxd/modmode/listeners/JoinListener.java
@@ -8,6 +8,7 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
 
 public class JoinListener implements Listener {
     private final ModModePlugin plugin;
@@ -23,6 +24,19 @@ public class JoinListener implements Listener {
                 User user = plugin.getLuckPerms().getUserManager().getUser(p.getUniqueId());
                 player.sendMessage(ChatColor.translateAlternateColorCodes('&',
                         "&5&l[ѕᴛᴀꜰꜰ] &r" + user.getCachedData().getMetaData().getSuffix() + p.getName() + " &7ᴄᴏɴɴᴇᴄᴛᴇᴅ ᴛᴏ &5&lѕᴍᴘ"));
+            }
+        }
+    }
+
+    @EventHandler
+    public void onLeave(PlayerQuitEvent e) {
+        Player p = e.getPlayer();
+        e.setQuitMessage("");
+        for (Player player : Bukkit.getOnlinePlayers()) {
+            if (player.hasPermission("daxel.staff")) {
+                User user = plugin.getLuckPerms().getUserManager().getUser(p.getUniqueId());
+                player.sendMessage(ChatColor.translateAlternateColorCodes('&',
+                        "&5&l[ѕᴛᴀꜰꜰ] &r" + user.getCachedData().getMetaData().getSuffix() + p.getName() + " &7ᴅɪѕᴄᴏɴɴᴇᴄᴛᴇᴅ ꜰʀᴏᴍ  &5&lѕᴍᴘ"));
             }
         }
     }
