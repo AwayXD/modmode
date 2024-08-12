6d8f8b93d312f43ab05d749a0aa1bbbcb0be2e69
Stryff <cjkester8@gmail.com>
2024-08-12 01:56:37 -0400

    cock

--- src/main/java/org/awayxd/modmode/listeners/JoinListener.java
+++ src/main/java/org/awayxd/modmode/listeners/JoinListener.java
@@ -6,9 +6,7 @@ import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
-import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
-import org.bukkit.event.player.AsyncPlayerChatEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
 
 public class JoinListener implements Listener {
